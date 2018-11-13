b03176f0614ad3c143a821da4d57b08b6e2bcd05
郭宇鹏 <1933304128@qq.com>
2018-11-13 15:15:34 +0800

    第一次laravell框架提交

---
+++ app/Http/Controllers/Edu/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Edu;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    public function index(){
+        dd('hworld');
+    }
+}
